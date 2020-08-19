04f277812aaaca987f67ec7aade071c1f84b8e6c
Tony Su <peitaosu@163.com>
2020-08-19 21:44:44 +0800

    add sample file

---
+++ Sample.cpp
@@ -0,0 +1,12 @@
+#include "Options.h"
+
+int main(int argc, char **argv)
+{
+    Options opts(argc, argv);
+    if (!opts.check("--arg1") && !opts.check("--arg2")) {
+        std::cout << "--arg1 and --arg2 are required." << std::endl;
+        return -1;
+    }
+    string arg1 = opts.get_value("--arg1");
+    return 0;
+}
