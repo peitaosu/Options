67605e9e6356d996d383a9ab4eced85debc1c8ae
Tony Su <peitaosu@163.com>
2020-08-20 22:07:28 +0800

    add comments

--- Options.h
+++ Options.h
@@ -7,8 +7,11 @@ class Options
 public:
     Options(int argc, char **argv);
     ~Options();
+    //get argument value
     std::string get_value(const char *argv);
+    //get argument by index
     std::string get_argv(int index);
+    //check if argument provided
     int check(const char *argv);
 
 private:
